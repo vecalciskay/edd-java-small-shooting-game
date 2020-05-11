d9ff3af46104ae7d029dd1505205122f8afd48c0
Vladimir Calderon <vladimir.calderon@gmail.com>
2020-05-10 21:57:20 -0400

    constructor

--- src/shooter/gui/ShooterFrame.java
+++ src/shooter/gui/ShooterFrame.java
@@ -9,4 +9,14 @@ public class ShooterFrame extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public ShooterFrame() {
+		init();
+		
+		this.pack();
+		this.setVisible(true);
+	}
+	
+	public void init() {
+		
+	}
 }
