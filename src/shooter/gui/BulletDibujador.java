ac9c0ed7624ab52587408887be91a302de1092d3
Vladimir Calderon <vladimir.calderon@gmail.com>
2020-05-14 19:45:06 -0400

    rendering antialiasing

--- src/shooter/gui/BulletDibujador.java
+++ src/shooter/gui/BulletDibujador.java
@@ -2,6 +2,8 @@ package shooter.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import shooter.framework.IDibujador;
 import shooter.model.Bullet;
@@ -15,8 +17,12 @@ public class BulletDibujador implements IDibujador {
 	}
 	@Override
 	public void dibujar(Graphics g, int x, int y) {		
-		g.setColor(Color.red);
-		g.fillOval(x, y, Bullet.ANCHO_BULLET, Bullet.ANCHO_BULLET);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(Color.red);
+		g2.fillOval(x, y, Bullet.ANCHO_BULLET, Bullet.ANCHO_BULLET);
 	}
 
 }
--- src/shooter/gui/CanonDibujador.java
+++ src/shooter/gui/CanonDibujador.java
@@ -1,7 +1,10 @@
 package shooter.gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import shooter.framework.IDibujador;
 import shooter.model.Canon;
@@ -16,14 +19,19 @@ public class CanonDibujador implements IDibujador {
 
 	@Override
 	public void dibujar(Graphics g, int x, int y) {
-		g.setColor(Color.black);
+		Graphics2D g2 = (Graphics2D)g; 
+		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(Color.black);
 		int canonX = (int)(Math.cos((double)modelo.getGradoActualRadian()) * (double)Canon.RADIUS_CANON);
 		int canonY = (int)(Math.sin((double)modelo.getGradoActualRadian()) * (double)Canon.RADIUS_CANON);
-		g.drawLine(modelo.getX() + Canon.RADIUS_BASE, modelo.getY(), 
+		
+		g2.setStroke(new BasicStroke(5));
+		g2.drawLine(modelo.getX() + Canon.RADIUS_BASE, modelo.getY(), 
 				modelo.getX() + Canon.RADIUS_BASE + canonX, 
 				modelo.getY() - canonY);
 		
-		g.fillArc(modelo.getX(), modelo.getY() - Canon.RADIUS_BASE, 
+		g2.fillArc(modelo.getX(), modelo.getY() - Canon.RADIUS_BASE, 
 				Canon.RADIUS_BASE * 2, Canon.RADIUS_BASE * 2, 0, 180);
 	}
 
